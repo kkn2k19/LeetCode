e346a15c73ca0a7e0b6b36936438f7fc30dbb549
kkn2k19 <karankumarnonia11@gmail.com>
2025-01-24 23:45:45 +0530

    Easy 26 - Remove Duplicates from Sorted Array

---
+++ E_26_RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,78 @@
+// 24.01.2025
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+// 26. Remove Duplicates from Sorted Array
+
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+// Custom Judge:
+
+// The judge will test your solution with the following code:
+
+// int[] nums = [...]; // Input array
+// int[] expectedNums = [...]; // The expected answer with correct length
+
+// int k = removeDuplicates(nums); // Calls your implementation
+
+// assert k == expectedNums.length;
+// for (int i = 0; i < k; i++) {
+//     assert nums[i] == expectedNums[i];
+// }
+// If all assertions pass, then your solution will be accepted.
+
+// Example 1:
+
+// Input: nums = [1,1,2]
+// Output: 2, nums = [1,2,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+// Example 2:
+
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class E_26_RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length of Array : ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.print("Enter elements of Array : ");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println("Array Provided : " + Arrays.toString(nums));
+        System.out.println("Number of unique elements: " + removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int index = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[index]) {
+                index++;
+                nums[index] = nums[i];
+            }
+        }
+        return index + 1;
+    }
+}
+
+// OUTPUT -----
+
+// Enter length of Array : 10
+// Enter elements of Array : 0 0 1 1 1 2 2 3 3 4
+// Array Provided : [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+// Number of unique elements: 5
