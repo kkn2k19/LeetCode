d7e84b60d269ab9f2159c19a79aafab4c579b470
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-10-13 15:34:22 +0530

    .

---
+++ M_75_SortColors.java
@@ -0,0 +1,37 @@
+// 13.10.2025
+// https://leetcode.com/problems/sort-colors/
+// 75. Sort Colors
+
+public class M_75_SortColors {
+    public static void sortColors(int[] nums) {
+        int start = 0; // index for zeroes
+        int mid = 0; // points to current element
+        int end = nums.length - 1; // index for 2s
+
+        while (mid <= end) {
+            if (nums[mid] == 0) {
+                int temp = nums[start];
+                nums[start] = nums[mid];
+                nums[mid] = temp;
+                mid += 1;
+                start += 1;
+            } else if (nums[mid] == 1) {
+                mid += 1; // no need to do anything for 1 when 0 and 2 got sorted 1 will automatically
+                          // come to real places
+            } else if (nums[mid] == 2) {
+                int temp = nums[end];
+                nums[end] = nums[mid];
+                nums[mid] = temp;
+                end -= 1;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = { 2, 0, 2, 1, 1, 0 };
+        sortColors(nums1);
+
+        int[] nums2 = { 2, 0, 1 };
+        sortColors(nums2);
+    }
+}
