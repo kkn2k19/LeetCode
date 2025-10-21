28e4beb8bd7712efdf100890b04bfa62f642c087
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-10-22 01:42:49 +0530

    .

--- E_67_AddBinary.java
+++ E_67_AddBinary.java
@@ -5,31 +5,7 @@
 import java.util.*;
 
 public class E_67_AddBinary {
-    /* 
-    public static String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder("");
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-        int carry = 0;
-
-        while (i >= 0 || j >= 0 || carry == 1) {
-            int sum = carry;
-            if (i >= 0) {
-                sum += a.charAt(i) - '0';
-                i -= 1;
-            }
-            if (j >= 0) {
-                sum += b.charAt(j) - '0';
-                j -= 1;
-            }
-
-            sb.append(sum % 2);
-            carry = sum / 2;
-        }
-        return sb.reverse().toString();
-    }
-    */
-
+    /*
     public static String addBinary(String a, String b) {
         int size = Math.max(a.length(), b.length());
         char[] res = new char[size + 1]; // 1 extra size for carry if there !!
@@ -57,6 +33,30 @@ public class E_67_AddBinary {
             return new String(res, 1, res.length - 1);
         }
         return new String(res);
+    }
+     */
+
+    public static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder("");
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i -= 1;
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j -= 1;
+            }
+
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        return sb.reverse().toString();
     }
 
     public static void main(String[] args) {
