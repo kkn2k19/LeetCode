46c9d7af86616b14bf005ecc8a4a89435a90b797
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-10-22 01:32:35 +0530

    .

--- E_67_AddBinary.java
+++ E_67_AddBinary.java
@@ -3,6 +3,7 @@
 // 67. Add Binary
 
 public class E_67_AddBinary {
+    /* 
     public static String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder("");
         int i = a.length() - 1;
@@ -24,6 +25,35 @@ public class E_67_AddBinary {
             carry = sum / 2;
         }
         return sb.reverse().toString();
+    }
+    */
+
+    public static String addBinary(String a, String b) {
+        int size = Math.max(a.length(), b.length());
+        char[] res = new char[size + 1]; // 1 extra size for carry if there !!
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int k = res.length - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i -= 1;
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j -= 1;
+            }
+            res[k] = (char) ((sum % 2) + '0');
+            k -= 1;
+            carry = sum / 2;
+        }
+        if (res[0] == '0') {
+            return new String(res, 1, res.length - 1);
+        }
+        return new String(res);
     }
 
     public static void main(String[] args) {
