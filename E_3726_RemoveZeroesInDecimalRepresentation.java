45017ec0726c65ea1354f94f43e15b9a91dd5871
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-10-26 23:52:58 +0530

    .

---
+++ E_3726_RemoveZeroesInDecimalRepresentation.java
@@ -0,0 +1,34 @@
+// 26.10.2025
+// https://leetcode.com/problems/remove-zeros-in-decimal-representation/description/
+// 3726. Remove Zeros in Decimal Representation
+
+import java.util.*;
+
+public class E_3726_RemoveZeroesInDecimalRepresentation {
+    public static long removeZeroes(long n) {
+        StringBuilder sb = new StringBuilder("");
+        while (n > 0) {
+            int d = (int) (n % 10);
+            if (d != 0) {
+                sb.append(d);
+            }
+            n /= 10;
+        }
+        return Long.parseLong(sb.reverse().toString());
+    }
+
+    public static void main(String[] args) {
+        long n1 = 1020030;
+        System.out.println(removeZeroes(n1));
+
+        long n2 = 1;
+        System.out.println(removeZeroes(n2));
+    }
+}
+
+// E:\Karan_codes\LeetCode>cd "e:\Karan_codes\LeetCode\" && javac
+// E_3726_RemoveZeroesInDecimalRepresentation.java && java
+// E_3726_RemoveZeroesInDecimalRepresentation && del
+// E_3726_RemoveZeroesInDecimalRepresentation.class
+// 123
+// 1
